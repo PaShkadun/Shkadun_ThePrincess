01509c76eb5057e43a09f11417475ba4e62bff14
rbisb <rbisb@DESKTOP-MM26GKA>
2020-11-21 18:22:58 +0300

    Load Project Code

---
+++ Shkadun_ThePrincess/Mine.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Shkadun_ThePrincess
+{
+    class Mine
+    {
+        public string Status { get; private set; }
+        public int Damage { get; private set; }
+
+        public void InactiveMine()
+        {
+            Status = "Inactive";
+        }
+
+        public Mine(int damage)
+        {
+            Status = "Active";
+            Damage = damage;
+        }
+    }
+}
